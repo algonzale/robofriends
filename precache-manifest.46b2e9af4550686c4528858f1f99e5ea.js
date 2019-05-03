d911e057f469ef5e12c3cecd4ae945c51ae83f35
Alejandro Jimenez <ajim4268@github.com>
2019-05-02 19:59:52 -0600

    Updates

---
+++ precache-manifest.46b2e9af4550686c4528858f1f99e5ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "f87a89208454f5ab1183",
+    "url": "/robofriends/static/js/main.f87a8920.chunk.js"
+  },
+  {
+    "revision": "8c6a9eff376dd70d3f95",
+    "url": "/robofriends/static/js/1.8c6a9eff.chunk.js"
+  },
+  {
+    "revision": "f87a89208454f5ab1183",
+    "url": "/robofriends/static/css/main.7568aaff.chunk.css"
+  },
+  {
+    "revision": "8c6a9eff376dd70d3f95",
+    "url": "/robofriends/static/css/1.c37f1b4b.chunk.css"
+  },
+  {
+    "revision": "d3df4ea47f46cf9601b8c5f53d19ad34",
+    "url": "/robofriends/index.html"
+  }
+];
